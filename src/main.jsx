9f88c7e133bfb5389b1a05257fd8ca16a5db4686
Alamin0x01 <md.alamin.ali.096@gmail.com>
2023-06-14 16:17:30 +0600

    banner title added

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/MainRoute";
 import AuthProvider from "./provider/AuthProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HelmetProvider } from "react-helmet-async";
-// import bgAnimation from "../src/assets/bg-animation1.json";
-import bgAnimation0 from "../src/assets/bg-animation.json";
-import Lottie from "lottie-react";
+import "./index.css";
 const queryClient = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div>
-    <div className="w-full  h-full sm:h-full absolute">
-      {/* <Lottie animationData={bgAnimation} loop={true} /> */}
-    </div>
-    <div className="w-full mt-96 h-full sm:h-full absolute">
-      <Lottie animationData={bgAnimation0} loop={true} />
-    </div>
     <React.StrictMode>
       <AuthProvider>
         <HelmetProvider>
