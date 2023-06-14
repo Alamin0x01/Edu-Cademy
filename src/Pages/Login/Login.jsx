bf2403908c3c536081ba7175ac8bf315ee9d259f
Alamin0x01 <md.alamin.ali.096@gmail.com>
2023-06-14 22:04:13 +0600

    password hide icon-btn --  added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -7,14 +7,14 @@ import { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import SectionTitle from "../Shared/SectionTitle";
 import { Helmet } from "react-helmet-async";
+import reg from "../../../src/assets/register-icon.json";
+import Lottie from "lottie-react";
 
 const Login = () => {
   const { loginUser } = useAuth();
   const navigate = useNavigate();
   const location = useLocation();
-
   const from = location.state?.from?.pathname || "/";
-
   const [showPassword, setShowPassword] = useState(false);
 
   const togglePasswordVisibility = () => {
@@ -55,62 +55,70 @@ const Login = () => {
         <title>Login | Edu-Cademy</title>
       </Helmet>
       <SectionTitle title="Login" />
-      <div className="hero min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse justify-evenly">
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  {...register("email", { required: true })}
-                  type="email"
-                  placeholder="Your email"
-                  className="input input-bordered"
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <div className="flex items-center">
+      <div className="flex gap-40 mb-8 ml-48">
+        <div className="w-96">
+          <Lottie animationData={reg} loop={true} />
+        </div>
+        <div>
+          <div className=" flex-col ">
+            <div className="card shadow-2xl bg-base-100">
+              <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
                   <input
-                    {...register("password", { required: true })}
-                    type={showPassword ? "text" : "password"}
-                    placeholder="********"
+                    {...register("email", { required: true })}
+                    type="email"
+                    placeholder="Your email"
                     className="input input-bordered"
                   />
-                  <button
-                    type="button"
-                    className="ms-2 text-2xl text-primary"
-                    onClick={togglePasswordVisibility}
-                  >
-                    {!showPassword ? (
-                      <FaEye></FaEye>
-                    ) : (
-                      <FaEyeSlash></FaEyeSlash>
-                    )}
-                  </button>
                 </div>
-                <label className="label">
-                  {errors.exampleRequired && (
-                    <span>This field is required</span>
-                  )}
-                </label>
-              </div>
-              <div className="form-control mt-6">
-                <input
-                  type="submit"
-                  value="Login"
-                  className="btn btn-primary w-9/12"
-                />
-              </div>
-            </form>
-            <p className="p-5 text-center">
-              New Here? <Link to="/register">Create an account</Link>{" "}
-            </p>
-            <SocialLogin></SocialLogin>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <div className="flex items-center">
+                    <input
+                      {...register("password", { required: true })}
+                      type={showPassword ? "text" : "password"}
+                      placeholder="********"
+                      className="input input-bordered"
+                    />
+                    <button
+                      type="button"
+                      className=" text-xl text-info absolute ml-44"
+                      onClick={togglePasswordVisibility}
+                    >
+                      {!showPassword ? (
+                        <FaEye></FaEye>
+                      ) : (
+                        <FaEyeSlash></FaEyeSlash>
+                      )}
+                    </button>
+                  </div>
+                  <label className="label">
+                    {errors.exampleRequired && (
+                      <span>This field is required</span>
+                    )}
+                  </label>
+                </div>
+                <div className="form-control ">
+                  <input
+                    type="submit"
+                    value="Login"
+                    className="btn btn-info "
+                  />
+                </div>
+              </form>
+              <p className=" text-center">
+                New Here?
+                <Link to="/register" className="text-cyan-400">
+                  Create an account
+                </Link>
+              </p>
+              <SocialLogin></SocialLogin>
+            </div>
           </div>
         </div>
       </div>
