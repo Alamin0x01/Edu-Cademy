2aac4ffa37aaa9badee73f8a61c3baf9ccee5c85
Alamin0x01 <md.alamin.ali.096@gmail.com>
2023-06-14 13:37:00 +0600

    full bg-animations added

--- src/main.jsx
+++ src/main.jsx
@@ -6,19 +6,29 @@ import router from "./Routes/MainRoute";
 import AuthProvider from "./provider/AuthProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HelmetProvider } from "react-helmet-async";
-
+import bgAnimation from "../src/assets/bg-animation1.json";
+import bgAnimation0 from "../src/assets/bg-animation.json";
+import Lottie from "lottie-react";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <HelmetProvider>
-        <QueryClientProvider client={queryClient}>
-          <div className="max-w-screen-xl mx-auto">
-            <RouterProvider router={router}></RouterProvider>
-          </div>
-        </QueryClientProvider>
-      </HelmetProvider>
-    </AuthProvider>
-  </React.StrictMode>
+  <div>
+    <div className="w-full  h-full sm:h-full absolute">
+      <Lottie animationData={bgAnimation} loop={true} />
+    </div>
+    <div className="w-full mt-96  h-full sm:h-full absolute">
+      <Lottie animationData={bgAnimation0} loop={true} />
+    </div>
+    <React.StrictMode>
+      <AuthProvider>
+        <HelmetProvider>
+          <QueryClientProvider client={queryClient}>
+            <div className="max-w-screen-xl mx-auto">
+              <RouterProvider router={router}></RouterProvider>
+            </div>
+          </QueryClientProvider>
+        </HelmetProvider>
+      </AuthProvider>
+    </React.StrictMode>
+  </div>
 );
