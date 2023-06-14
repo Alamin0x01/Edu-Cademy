242befbf5b5fa5afcd32573d880ba7e18d01c13c
Alamin0x01 <md.alamin.ali.096@gmail.com>
2023-06-14 16:40:04 +0600

    footer info update

--- src/Pages/Shared/Footer.jsx
+++ src/Pages/Shared/Footer.jsx
@@ -1,34 +1,93 @@
 import { Link } from "react-router-dom";
+import {
+  FaFacebook,
+  FaTwitter,
+  FaInstagramSquare,
+  FaGithub,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="footer mt-3 p-10 bg-base-200 text-base-content">
-      <div>
-        <img className="w-16" src="www.fd.hh" alt="" />
-        <p className="text-xl text-primary font-bold">Edu-Cademy</p>
-        <br />
-        <p className="font-bold">©Edu-Cademy 2023</p>
+    <footer className=" p-10 bg-sky-500 text-white">
+      <div className="flex justify-between flex-col lg:flex-row items-center text-center lg:space-y-0 space-y-6">
+        <div>
+          <img
+            className="h-12 ml-9"
+            src="https://i.ibb.co/bQxpnbc/of-HD-N1-QIFy-I4-AAAAASUVORK5-CYII.jpg"
+            alt=""
+          />
+          <p>
+            Edu-Cademy
+            <br />
+            support@Edu-Cademy.com <br />
+            +880 175634456
+          </p>
+        </div>
+        <div>
+          <span className="footer-title">Menu</span>
+          <p>
+            <Link className="hover:text-orange" to="/">
+              Home
+            </Link>
+          </p>
+          <p>
+            <Link className="hover:text-orange" to="/Classes">
+              Classes
+            </Link>
+          </p>
+          <p>
+            <Link className="hover:text-orange" to="/">
+              Teachers
+            </Link>
+          </p>
+          <p>
+            <Link className="hover:text-orange" to="/">
+              Dashboard
+            </Link>
+          </p>
+          <p>
+            <Link className="hover:text-orange" to="/">
+              Blogs
+            </Link>
+          </p>
+        </div>
+        <div>
+          <span className="footer-title">Address</span>
+          <p>
+            South air, <br />
+            Bonani Road <br />
+            Dhaka
+            <br />
+            1st floor, Edu-Cademy
+          </p>
+        </div>
+        <div>
+          <span className="footer-title">Social Links</span>
+          <div className="mb-4">
+            <div className="flex space-x-4">
+              <a href="https://www.facebook.com/Edu-Cademy">
+                <FaFacebook className="text-3xl cursor-pointer hover:text-orange" />
+              </a>
+              <a href="https://www.instagram.com/Edu-Cademy/">
+                <FaInstagramSquare className="text-3xl cursor-pointer hover:text-orange" />
+              </a>
+              <a href="https://twitter.com/Edu-Cademy">
+                <FaTwitter className="text-3xl cursor-pointer hover:text-orange" />
+              </a>
+              <a href="https://github.com/Edu-Cademy">
+                <FaGithub className="text-3xl cursor-pointer hover:text-orange" />
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
       <div>
-        <span className="footer-title">Links</span>
-        <Link to="/" className="link link-hover">
-          Home
-        </Link>
-        <Link to="/classes" className="link link-hover">
-          Classes
-        </Link>
-        <Link to="/instructors" className="link link-hover">
-          Instructors
-        </Link>
-        <Link to="/dashboard/home" className="link link-hover">
-          Dashboard
-        </Link>
-      </div>
-      <div>
-        <span className="footer-title">Legal</span>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
+        <div className=" items-center mt-8">
+          <div className="border-t border-orange mx-auto mb-6"></div>
+          <div className="text-center">
+            <p>Copyright © Edu-Cademy - 2023 - All right reserved</p>
+          </div>
+        </div>
       </div>
     </footer>
   );
