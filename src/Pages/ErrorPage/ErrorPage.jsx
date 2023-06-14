b444d1ce19e5367016b39211f9511d897afe3df8
Alamin0x01 <md.alamin.ali.096@gmail.com>
2023-06-14 12:14:58 +0600

    Lottie Animation with error page

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,6 +1,6 @@
 import { Link, useRouteError } from "react-router-dom";
 import Lottie from "lottie-react";
-import reader from "../../assets/render.json";
+import errorPage from "../../assets/Error-page.json";
 import { Helmet } from "react-helmet-async";
 const ErrorPage = () => {
   const { error } = useRouteError();
@@ -13,8 +13,8 @@ const ErrorPage = () => {
         <div className="container flex flex-col items-center justify-center">
           {/* Lottie Animation */}
           <div className="relative lg:w-1/2 ">
-            <div className="w-full lg:w-4/5 lg:ml-auto h-56 sm:h-96">
-              <Lottie animationData={reader} loop={true} />
+            <div className="w-full lg:w-4/5  h-auto sm:h-96">
+              <Lottie animationData={errorPage} loop={true} />
             </div>
           </div>
           <div className="text-center mt-52 md:pl-36">
@@ -22,7 +22,7 @@ const ErrorPage = () => {
               {error?.message}
             </p>
             <Link to="/">
-              <button className="btn btn-secondary md:mt-5">
+              <button className="btn btn-outline btn-info md:mt-5">
                 Back to homepage
               </button>
             </Link>
