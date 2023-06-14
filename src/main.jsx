f24e08d9e3b03e277077fdc48493a33ded23a401
Alamin0x01 <md.alamin.ali.096@gmail.com>
2023-06-14 16:18:59 +0600

    authprovider added

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/MainRoute";
 import AuthProvider from "./provider/AuthProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
+
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div>
