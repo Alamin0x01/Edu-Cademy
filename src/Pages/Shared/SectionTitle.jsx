5b2215571e192e5c6ffe881e3bb345d8b699578d
Alamin0x01 <md.alamin.ali.096@gmail.com>
2023-06-14 12:56:58 +0600

    section title styles added

--- src/Pages/Shared/SectionTitle.jsx
+++ src/Pages/Shared/SectionTitle.jsx
@@ -1,13 +1,12 @@
 const SectionTitle = ({ title }) => {
-  const hrColor = "rgb(0, 0, 255)"; // RGB value for blue
-
+  const hrColor = "rgb(227, 116, 216)";
   return (
     <div className="mx-auto text-center my-8">
       <hr
-        className={`border-t-2 border-solid border-blue-500 w-32 mx-auto mb-4`}
+        className={`border-t-2 border-solid border-t-red-600 w-32 mx-auto mb-4`}
         style={{ borderColor: hrColor }}
       />
-      <h3 className="text-3xl font-bold text-blue-500">{title}</h3>
+      <h3 className="text-3xl font-bold text-cyan-400">{title}</h3>
       <hr
         className={`border-t-2 border-solid border-blue-500 w-32 mx-auto mt-4`}
         style={{ borderColor: hrColor }}
