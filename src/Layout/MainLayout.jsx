35a69a770a8dcfd7c7ece0f8f0614cc11f3b5f33
Alamin0x01 <md.alamin.ali.096@gmail.com>
2023-06-14 22:28:06 +0600

    Footer page --  added

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,13 +1,14 @@
-import Footer from "../Pages/Shared/Footer";
 import NavBar from "../Pages/Shared/NavBar";
+import Footer from "../Pages/Shared/Footer";
 import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
     <div>
-      <NavBar />
+      <NavBar></NavBar>
       <Outlet />
-      <Footer />
+
+      <Footer></Footer>
     </div>
   );
 };
