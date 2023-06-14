9430f12ecda37d639228bfbd33e86ed0f295ff3e
Alamin0x01 <md.alamin.ali.096@gmail.com>
2023-06-15 00:13:13 +0600

    useAuth  added

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -3,7 +3,9 @@ import useAuth from "../hooks/useAuth";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useAuth();
+
   const location = useLocation();
+
   if (loading)
     return (
       <div className="flex justify-center items-center">
